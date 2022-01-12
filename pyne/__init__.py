3be048d1191f43b34e69f38801a9eded93a3f46c
nchistov <nikola-chistov@mail.ru>
2022-01-12 11:46:24 +0300

    add method run which start mainloop and method quit to close the window

--- pyne/__init__.py
+++ pyne/__init__.py
@@ -7,10 +7,11 @@ __version__ = '0.1.0'
 
 class App:
     """Class App, it is a class which manages windows"""
-    def __init__(self, window_size=(500, 500), title="Pyne"):
+    def __init__(self, window_size=(500, 500), title="Pyne", bg_color=(255, 255, 255)):
         """
-        :param window_size: it is a list or a tuple of two numbers: first width and last height
-        :param title: it is a title of the window
+        :param window_size: list or tuple of two numbers: first width and last height
+        :param title: title of the window
+        :param bg_color: color of background, in format RGB
         """
         pg.init()
         print("\nWelcome to the Pyne")
@@ -20,4 +21,25 @@ class App:
         self.screen = pg.display.set_mode(window_size)
         pg.display.set_caption(title)
 
+        self.clock = pg.time.Clock()
+
         self.time_table = []
+        self.running = False
+        self.bg = bg_color
+        self.fps = 30
+
+    def quit(self):
+        self.running = False
+        pg.quit()
+
+    def run(self):
+        self.running = True
+        while self.running:
+            self.screen.fill(self.bg)
+
+            for business in self.time_table:
+                business()
+
+            pg.display.flip()
+
+            self.clock.tick()

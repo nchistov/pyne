b694af8bffa7f2d7d9bea28fdc47ddd0289dbe8f
nchistov <nikola-chistov@mail.ru>
2022-01-15 12:17:17 +0300

    add file grid.py

---
+++ pyne/grid.py
@@ -0,0 +1,14 @@
+class Grid:
+    def __init__(self, rows, columns):
+        self.rows = rows
+        self.columns = columns
+
+    def add_widget(self, widget, row, column):
+        try:
+            widget.max_rows = self.rows
+            widget.max_columns = self.columns
+
+            widget.row = row
+            widget.column = column
+        except AttributeError:
+            raise AttributeError("Widget must have got attributes max_rows, max_columns, row and column")

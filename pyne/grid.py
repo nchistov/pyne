6bc67abf69df859800a9d5018a0639133f08af77
nchistov <nikola-chistov@mail.ru>
2022-01-16 11:56:21 +0300

    add check correct pos

--- pyne/grid.py
+++ pyne/grid.py
@@ -1,9 +1,17 @@
+class NoSouchPositionError(Exception): pass
+
+
 class Grid:
     def __init__(self, rows, columns):
         self.rows = rows
         self.columns = columns
 
     def add_widget(self, widget, row, column):
+        if row >= self.rows:
+            raise NoSouchPositionError("Row must be less then max rows")
+        if column >= self.columns:
+            raise NoSouchPositionError("Column must be less then max columns")
+
         try:
             widget.max_rows = self.rows
             widget.max_columns = self.columns

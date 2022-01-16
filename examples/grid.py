45273fedcfbeec3823d90605b8cd61460b9350b9
nchistov <nikola-chistov@mail.ru>
2022-01-16 12:03:29 +0300

    add second button

--- examples/grid.py
+++ examples/grid.py
@@ -2,13 +2,16 @@ from pyne.app import App
 from pyne.widgets import Button
 from pyne.grid import Grid
 
-btn = Button('Hello', command=lambda: print('Hello!'))
+btn1 = Button('1', command=lambda: print('Hello from btn1!'))
+btn2 = Button('2', command=lambda: print('Hello from btn2!'))
 
-grid = Grid(3, 3)
-grid.add_widget(btn, 1, 1)
+grid = Grid(5, 5)
+grid.add_widget(btn1, 1, 0)
+grid.add_widget(btn2, 1, 1)
 
 app = App()
 
-app.add_widget(btn)
+app.add_widget(btn1)
+app.add_widget(btn2)
 
 app.run()

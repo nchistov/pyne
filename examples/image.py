c52fa7a1b6188bfaaf74eabc11351656fa169ece
nchistov <nikola-chistov@mail.ru>
2022-01-24 12:26:04 +0300

    change this example for locating image in the center of window

--- examples/image.py
+++ examples/image.py
@@ -1,9 +1,13 @@
 from pyne.app import App
+from pyne.grid import Grid
+
 from pyne.image import Image
 
 app = App()
 
+grid = Grid(app, 3, 3)
+
 image = Image(app, 'image.png')
-app.add_to_schedule(image.draw)
+grid.add_image(image, 1, 1)
 
 app.run()

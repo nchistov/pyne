55d3f282363ee7577de0d18d694148da6c9ed3ff
nchistov <nikola-chistov@mail.ru>
2022-01-28 14:16:55 +0300

    add opening files if this examples runs in other directories

--- examples/image.py
+++ examples/image.py
@@ -1,3 +1,5 @@
+import os
+
 from pyne import App
 from pyne.grid import Grid
 
@@ -7,8 +9,8 @@ app = App()
 
 grid = Grid(app, 3, 3)
 
-image = Image(app, 'image.png')
-image2 = Image(app, 'image.png')
+image = Image(app, os.path.join(os.path.dirname(__file__), 'image.png'))
+image2 = Image(app, os.path.join(os.path.dirname(__file__), 'image.png'))
 
 grid.add_image(image, 0, 0)
 grid.add_image(image2, 1, 1, size_correction=True)

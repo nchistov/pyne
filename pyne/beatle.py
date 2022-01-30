e2041a263b2a92737b430f3d820fa2532c984ac5
nchistov <nikola-chistov@mail.ru>
2022-01-30 14:23:21 +0300

    add method update and multiprocessing

--- pyne/beatle.py
+++ pyne/beatle.py
@@ -1,5 +1,7 @@
 import os.path
 from math import sin, cos, radians
+from threading import Thread
+from time import sleep
 
 import pygame as pg
 
@@ -16,21 +18,25 @@ class Beatle:
         self.image = pg.image.load(os.path.join(os.path.dirname(__file__), 'beatle.png'))
 
         self.rect = self.image.get_rect()
-        self.rect.centerx = beatle_screen.rect.centerx
-        self.rect.centery = beatle_screen.rect.centery
+        self.rect.center = beatle_screen.rect.center
 
         self.speed = 5
         self.angle = 0
+        self.steps = 0
 
         self.end_point = None
 
         self.moving = False
 
-    def forward(self, steps):
-        self.dx = self.speed * cos(radians(self.angle))
-        self.dy = self.speed * sin(radians(self.angle))
+        t = Thread(target=self.update)
+        self._running = True
+        t.start()
+        self.thread = t
 
-        self.end_point = (steps * cos(radians(self.angle)), steps * sin(radians(self.angle)))
+        self.app.add_to_schedule(self.draw)
+
+    def forward(self, steps):
+        self.steps = steps
 
         self.moving = True
 
@@ -39,12 +45,22 @@ class Beatle:
 
         self.image = pg.transform.rotate(self.image, self.angle)
 
+    def update(self):
+        while self._running:
+            if self.moving:
+                dx = self.speed * cos(radians(self.angle))
+                dy = self.speed * sin(radians(self.angle))
+                for i in range(self.steps // self.speed):
+                    self.rect.x += dx
+                    self.rect.y += dy
+
+                    sleep(0.02)
+                self.moving = False
+
+            sleep(0.02)
+
+    def stop(self):
+        self._running = False
+
     def draw(self):
-        if self.moving and self.rect.center != self.end_point:
-            self.rect.x += self.dx
-            self.rect.x += self.dy
-
-            self.x += self.dx
-            self.y += self.dy
-
-        self.screen.blit(self.image, self.rect)
+        self.app.screen.blit(self.image, self.rect)

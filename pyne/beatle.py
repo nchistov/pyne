1823335a3ee191099a3bbf1cf7b921672a3ebcd7
nchistov <nikola-chistov@mail.ru>
2022-02-02 12:44:22 +0300

    add queue tasks

--- pyne/beatle.py
+++ pyne/beatle.py
@@ -1,5 +1,6 @@
 import os.path
 from math import sin, cos, radians
+from queue import Queue
 from threading import Thread
 from time import sleep
 
@@ -15,7 +16,8 @@ class Beatle:
         self.x = 0
         self.y = 0
 
-        self.image = pg.image.load(os.path.join(os.path.dirname(__file__), 'beatle.png'))
+        self.base_image = pg.image.load(os.path.join(os.path.dirname(__file__), 'beatle.png'))
+        self.image = self.base_image
 
         self.rect = self.image.get_rect()
         self.rect.center = beatle_screen.rect.center
@@ -26,6 +28,8 @@ class Beatle:
 
         self.moving = False
 
+        self.tasks = Queue(maxsize=10000)
+
         t = Thread(target=self.update)
         self._running = True
         t.start()
@@ -34,30 +38,35 @@ class Beatle:
         self.app.add_to_schedule(self.draw)
 
     def forward(self, steps):
-        self.steps = steps
-
-        self.moving = True
-
-    def left(self, angle):
-        self.angle -= angle
+        self.tasks.put(('fd', steps))
 
     def setheading(self, angle):
-        self.angle = angle
-        self.image = pg.transform.rotate(self.image, self.angle)
+        self.tasks.put(('seth', angle))
 
     def update(self):
         while self._running:
-            if self.moving:
-                dx = self.speed * cos(radians(self.angle))
-                dy = self.speed * sin(radians(self.angle))
-                for i in range(self.steps // self.speed):
-                    self.rect.x += dx
-                    self.rect.y -= dy
+            if not self.tasks.empty():
+                task = self.tasks.get()
 
-                    self.x += dx
-                    self.y -= dy
+                if task[0] == 'fd':
+                    dx = self.speed * cos(radians(self.angle))
+                    dy = self.speed * sin(radians(self.angle))
+                    for i in range(task[1] // self.speed):
+                        self.rect.x += dx
+                        self.rect.y -= dy
 
-                    sleep(0.02)
+                        self.x += dx
+                        self.y -= dy
+
+                        sleep(0.02)
+
+                elif task[0] == 'seth':
+                    self.angle = task[1]
+
+                    self.angle %= 360
+
+                    self.image = pg.transform.rotate(self.base_image, self.angle)
+
                 self.moving = False
 
             sleep(0.02)

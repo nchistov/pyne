272ce1f71cd1d9bd88897aec259f498e01c410b3
nchistov <nikola-chistov@mail.ru>
2022-02-18 12:26:51 +0300

    add widget Panel

---
+++ pyne/widgets/panel.py
@@ -0,0 +1,21 @@
+import pygame as pg
+
+from .base_widget import Widget
+
+
+class Panel(Widget):
+    def __init__(self, color=(255, 255, 255), outline_color=(255, 255, 255)):
+        super().__init__()
+
+        self.color = color
+        self.outline_color = outline_color
+
+    def draw(self, screen: pg.Surface):
+        pg.draw.rect(screen, self.color, self.rect)
+
+        # Draw outline
+        pg.draw.line(screen, self.outline_color, (self.rect.right, self.rect.bottom), (self.rect.right, self.rect.top))
+        pg.draw.line(screen, self.outline_color, (self.rect.right, self.rect.top), (self.rect.left, self.rect.top))
+        pg.draw.line(screen, self.outline_color, (self.rect.left, self.rect.top), (self.rect.left, self.rect.bottom))
+        pg.draw.line(screen, self.outline_color, (self.rect.left, self.rect.bottom),
+                     (self.rect.right, self.rect.bottom))

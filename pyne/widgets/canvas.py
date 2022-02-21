a36c52b8f796ba5de82394ba98d1826816aa84c2
nchistov <nikola-chistov@mail.ru>
2022-02-21 12:33:43 +0300

    rename method point to draw_point and add methods draw_line, draw_rect and draw_circle

--- pyne/widgets/canvas.py
+++ pyne/widgets/canvas.py
@@ -13,8 +13,21 @@ class Canvas(Widget):
 
         self.objects = []
 
-    def point(self, x: int, y: int, color=(0, 0, 0)):
+    def draw_point(self, x: int, y: int, color=(0, 0, 0)):
         new_obj = CanvasObject(('point', (self.rect.x + x, self.rect.y + y), color))
+        self.objects.append(new_obj)
+
+    def draw_line(self, x1, y1, x2, y2, color=(0, 0, 0), width=1):
+        new_obj = CanvasObject(('line', (self.rect.x + x1, self.rect.y + y1), (self.rect.x + x2, self.rect.y + y2),
+                                color, width))
+        self.objects.append(new_obj)
+
+    def draw_rect(self, x, y, width, height, color=(0, 0, 0)):
+        new_obj = CanvasObject(('rect', (self.rect.x + x, self.rect.y + y), width, height, color))
+        self.objects.append(new_obj)
+
+    def draw_circle(self, x, y, R, color=(0, 0, 0)):
+        new_obj = CanvasObject(('circle', (self.rect.x + x, self.rect.y + y), R, color))
         self.objects.append(new_obj)
 
     def draw(self, screen: pg.Surface):

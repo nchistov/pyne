5327e5d6a5ca50ceebde9ded4673709d65e1d90c
nchistov <nikola-chistov@mail.ru>
2022-02-21 12:47:44 +0300

    add new possibility: adding sub grids

--- pyne/grid.py
+++ pyne/grid.py
@@ -6,11 +6,17 @@ class NoSouchPositionError(Exception):
 
 
 class Grid:
-    def __init__(self, app, rows, columns):
+    def __init__(self, app, rows, columns, x=0, y=0, width=None, height=None):
         self.app = app
 
         self.rows = rows
         self.columns = columns
+
+        self.x = x
+        self.y = y
+
+        self.width = width or app.screen.get_width()
+        self.height = height or app.screen.get_height()
 
     def add_widget(self, widget, row, column, width=1, height=1):
         if row >= self.rows:
@@ -19,17 +25,19 @@ class Grid:
             raise NoSouchPositionError("Column must be less then max columns")
 
         try:
-            sell_height = self.app.screen.get_height() / self.rows
-            sell_width = self.app.screen.get_width() / self.columns
+            sell_height = self.height / self.rows
+            sell_width = self.width / self.columns
 
-            widget.rect.x = column * sell_width
-            widget.rect.y = row * sell_height
+            widget.rect.x = self.x + (column * sell_width)
+            widget.rect.y = self.y + (row * sell_height)
 
             widget.rect.width = sell_width * width
             widget.rect.height = sell_height * height
 
         except AttributeError:
             raise AttributeError("Widget must has attribute rect")
+        except ZeroDivisionError:
+            pass
 
         self.app.add_widget(widget)
 

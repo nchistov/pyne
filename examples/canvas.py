0cb05708539a20ce6f5bdd12cd9613ed6d5d03d6
nchistov <nikola-chistov@mail.ru>
2022-02-22 15:11:32 +0300

    change example canvas.py

--- examples/canvas.py
+++ examples/canvas.py
@@ -11,5 +11,6 @@ canvas.draw_line(10, 10, 25, 25, (255, 0, 0))
 canvas.draw_line(200, 200, 250, 255, (255, 0, 0), 5)
 canvas.draw_rect(30, 50, 50, 200, (0, 0, 255))
 canvas.draw_circle(150, 150, 10, (255, 255, 0))
+canvas.draw_image('image.png', 250, 300)
 
 app.run()

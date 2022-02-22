6d0bf9b45461c104c0046b5963695351fab876c5
nchistov <nikola-chistov@mail.ru>
2022-02-22 15:15:01 +0300

    remove methods add_image and change_pos_of_image

--- pyne/grid.py
+++ pyne/grid.py
@@ -44,30 +44,3 @@ class Grid:
     def change_pos_of_widget(self, widget, new_row, new_column, new_width=0, new_height=0):
         self.app.remove_widget(widget)
         self.add_widget(widget, new_row, new_column, new_width, new_height)
-
-    def add_image(self, image, row, column, width=1, height=1, size_correction=False):
-        if row >= self.rows:
-            raise NoSouchPositionError("Row must be less then max rows")
-        if column >= self.columns:
-            raise NoSouchPositionError("Column must be less then max columns")
-
-        try:
-            sell_height = self.app.screen.get_height() / self.rows
-            sell_width = self.app.screen.get_width() / self.columns
-
-            image.rect.x = column * sell_width
-            image.rect.y = row * sell_height
-
-            image.rect.width = sell_width * width
-            image.rect.height = sell_height * height
-
-            if size_correction:
-                image.image = pg.transform.smoothscale(image.image, (sell_width, sell_height))
-
-        except AttributeError:
-            raise AttributeError("Widget must has attribute rect")
-
-        self.app.add_to_schedule(image.draw)
-
-    def change_pos_of_image(self, image, row, column, width=1, height=1, size_correction=False):
-        self.add_image(image, row, column, width, height, size_correction)

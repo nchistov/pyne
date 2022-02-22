8d3761235e81a1023fc7ed961455398913ea81a8
nchistov <nikola-chistov@mail.ru>
2022-02-22 14:56:40 +0300

    add type of arguments in widget Canvas

--- pyne/widgets/canvas.py
+++ pyne/widgets/canvas.py
@@ -17,16 +17,16 @@ class Canvas(Widget):
         new_obj = CanvasObject(('point', (self.rect.x + x, self.rect.y + y), color))
         self.objects.append(new_obj)
 
-    def draw_line(self, x1, y1, x2, y2, color=(0, 0, 0), width=1):
+    def draw_line(self, x1: int, y1: int, x2: int, y2: int, color=(0, 0, 0), width=1):
         new_obj = CanvasObject(('line', (self.rect.x + x1, self.rect.y + y1), (self.rect.x + x2, self.rect.y + y2),
                                 color, width))
         self.objects.append(new_obj)
 
-    def draw_rect(self, x, y, width, height, color=(0, 0, 0)):
+    def draw_rect(self, x: int, y: int, width: int, height: int, color=(0, 0, 0)):
         new_obj = CanvasObject(('rect', (self.rect.x + x, self.rect.y + y), width, height, color))
         self.objects.append(new_obj)
 
-    def draw_circle(self, x, y, R, color=(0, 0, 0)):
+    def draw_circle(self, x: int, y: int, R: int, color=(0, 0, 0)):
         new_obj = CanvasObject(('circle', (self.rect.x + x, self.rect.y + y), R, color))
         self.objects.append(new_obj)
 

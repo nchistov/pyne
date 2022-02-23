af9bfd0018a2adf1cb2ab0b35a49ea36a3f99675
nchistov <nikola-chistov@mail.ru>
2022-02-23 13:40:35 +0300

    add example moving_btn.py

---
+++ examples/moving_btn.py
@@ -0,0 +1,13 @@
+import pyne
+
+app = pyne.App()
+grid = pyne.widgets.Grid(app, 3, 3)
+
+app.add_widget(grid)
+
+btn = pyne.widgets.Button('OK', command=lambda: print('OK'))
+grid.add_widget(btn, 0, 0)
+
+grid.change_pos_of_widget(btn, 2, 2)
+
+app.run()

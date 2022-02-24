d1e64262068d1fc1a25d4de5b9eb55b43f4cf398
nchistov <nikola-chistov@mail.ru>
2022-02-24 14:37:51 +0300

    add example priority

---
+++ examples/priority.py
@@ -0,0 +1,14 @@
+import pyne
+
+app = pyne.App()
+grid = pyne.widgets.Grid(app, 3, 3)
+
+app.add_widget(grid)
+
+panel1 = pyne.widgets.Panel(color=(0, 0, 0))
+panel2 = pyne.widgets.Panel(color=(255, 0, 0))
+
+grid.add_widget(panel1, 0, 0, 2, 2)
+grid.add_widget(panel2, 1, 1, 2, 2, priority=0)
+
+app.run()

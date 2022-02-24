a1e6737fec7434b5f4cafb1d73f2f3bfd99c6e4f
nchistov <nikola-chistov@mail.ru>
2022-02-24 14:44:10 +0300

    excuse me now version not beta, it's still alpha

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Pyne
 ## Python application lib
-## version 0.1.0 beta
+## version 0.1.0 alpha
 
 ### Warning, python version must be 3.10 or high!
 
--- pyne/__init__.py
+++ pyne/__init__.py
@@ -8,7 +8,7 @@ from . import widgets
 
 from . import beatle
 
-__version__ = '0.1.0 beta'
+__version__ = '0.1.0 alpha'
 
 print("\nWelcome to the Pyne")
 print(f"Pyne {__version__} (pygame {pg.__version__}, "

ff102fd9f4aeb970befabd7010347e5680cbbfca
nchistov <nikola-chistov@mail.ru>
2022-02-26 12:12:00 +0300

    change example to last commit

--- examples/button_with_image.py
+++ examples/button_with_image.py
@@ -5,7 +5,7 @@ grid = pyne.widgets.Grid(app, 3, 3)
 
 app.add_widget(grid)
 
-btn = pyne.widgets.Button('OK', command=lambda: print('OK'), image='image.png')
+btn = pyne.widgets.Button('OK', command=lambda: print('OK'), image='image.png', sound='pop.wav')
 
 grid.add_widget(btn, 1, 1)
 

bd10bd815685b5c0f254afcf9746cf954d478e5c
nchistov <nikola-chistov@mail.ru>
2022-02-26 11:56:34 +0300

    add drawing image on Button and pressing it

--- pyne/widgets/button.py
+++ pyne/widgets/button.py
@@ -5,7 +5,7 @@ from .base_widget import Widget
 
 class Button(Widget):
     def __init__(self, text: str, color=(150, 150, 150), active_color=(70, 200, 215),
-                 text_color=(0, 0, 0), font_size=40, command: callable = None):
+                 text_color=(0, 0, 0), font_size=40, command: callable = None, image=None, press='left'):
         super().__init__()
         self.text = text
         self.color = color
@@ -13,9 +13,19 @@ class Button(Widget):
         self.text_color = text_color
         self.command = command
 
+        self.press = press
+
         self.current_color = color
 
         self.is_pressed = False
+
+        self.has_image = False
+
+        if image is not None:
+            self.image = pg.image.load(image)
+            self.image_rect = self.image.get_rect()
+
+            self.has_image = True
 
         self.rect = pg.Rect(0, 0, len(text) * 17, 35)
 
@@ -44,6 +54,38 @@ class Button(Widget):
 
         self.text_image_rect.center = self.rect.center
 
+        if self.has_image:
+            match self.press:
+                case 'right top':
+                    self.image_rect.top = self.rect.top
+                    self.image_rect.right = self.rect.right
+                case 'right':
+                    self.image_rect.centery = self.rect.centery
+                    self.image_rect.right = self.rect.right
+                case 'right bottom':
+                    self.image_rect.bottom = self.rect.bottom
+                    self.image_rect.right = self.rect.right
+                case 'center top':
+                    self.image_rect.top = self.rect.top
+                    self.image_rect.centerx = self.rect.centerx
+                case 'center':
+                    self.image_rect.centery = self.rect.centery
+                    self.image_rect.centerx = self.rect.centerx
+                case 'center bottom':
+                    self.image_rect.bottom = self.rect.bottom
+                    self.image_rect.centerx = self.rect.centerx
+                case 'left top':
+                    self.image_rect.top = self.rect.top
+                    self.image_rect.left = self.rect.left
+                case 'left':
+                    self.image_rect.centery = self.rect.centery
+                    self.image_rect.left = self.rect.left
+                case 'left bottom':
+                    self.image_rect.bottom = self.rect.bottom
+                    self.image_rect.left = self.rect.left
+                case _:
+                    raise ValueError('incorrect word to pressing image')
+
     def draw(self, screen):
         pg.draw.rect(screen, self.current_color, self.rect)
         screen.blit(self.text_image, self.text_image_rect)
@@ -55,3 +97,5 @@ class Button(Widget):
 
             pg.draw.line(screen, (200, 200, 200), (self.rect.right, self.rect.top),
                          (self.rect.left, self.rect.top))
+        if self.has_image:
+            screen.blit(self.image, self.image_rect)

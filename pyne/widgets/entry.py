775a699db3ec1bebddcd68a331dcad0bf578aab9
nchistov <nikola-chistov@mail.ru>
2022-02-27 12:09:01 +0300

    add method update and draw to widget Entry

--- pyne/widgets/entry.py
+++ pyne/widgets/entry.py
@@ -11,3 +11,22 @@ class Entry(Widget):
         self.outline_color = outline_color
 
         self.active = False
+
+    def update(self, event):
+        if event.type == pg.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = event.pos
+            if self.rect.collidepoint(mouse_x, mouse_y):
+                self.active = True
+
+    def draw(self, screen: pg.Surface):
+        pg.draw.rect(screen, self.bg_color, self.rect)
+
+        if self.active:
+            # Draw outline
+            pg.draw.line(screen, self.outline_color, (self.rect.right, self.rect.bottom),
+                         (self.rect.right, self.rect.top))
+            pg.draw.line(screen, self.outline_color, (self.rect.right, self.rect.top), (self.rect.left, self.rect.top))
+            pg.draw.line(screen, self.outline_color, (self.rect.left, self.rect.top),
+                         (self.rect.left, self.rect.bottom))
+            pg.draw.line(screen, self.outline_color, (self.rect.left, self.rect.bottom),
+                         (self.rect.right, self.rect.bottom))

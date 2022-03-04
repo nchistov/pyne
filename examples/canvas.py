c61bb28fbb06781c950ebc0971a33ba8acf304e2
nchistov <nikola-chistov@mail.ru>
2022-03-04 14:36:47 +0300

    change examples to last commit

--- examples/canvas.py
+++ examples/canvas.py
@@ -1,11 +1,11 @@
 import pyne
 
 app = pyne.App()
-grid = pyne.widgets.Grid(app, 1, 1)
+grid = pyne.widgets.Grid(app, 3, 3)
 app.add_widget(grid)
 
 canvas = pyne.widgets.Canvas((0, 255, 150))
-grid.add_widget(canvas, 0, 0)
+grid.add_widget(canvas, 0, 0, 2, 3)
 
 canvas.draw_point(5, 5, (255, 0, 0))
 canvas.draw_line(10, 10, 25, 25, (255, 0, 0))
@@ -13,7 +13,7 @@ canvas.draw_line(200, 200, 250, 255, (255, 0, 0), 5)
 canvas.draw_rect(30, 50, 50, 200, (0, 0, 255))
 canvas.draw_circle(150, 150, 10, (255, 255, 0))
 image = canvas.draw_image('image.png', 0, 350)
-canvas.draw_polygon(((400, 50), (450, 100), (400, 150)))
+canvas.draw_polygon(((300, 50), (350, 100), (300, 150)))
 canvas.draw_text('Test Canvas', 200, 150, 30)
 
 test_text = canvas.draw_text('Test Text', 200, 200, 30)

186ef713e59f9523c2faa4b59dd9966bea4950f5
nchistov <nikola-chistov@mail.ru>
2022-03-05 11:00:27 +0300

    change prompt

--- pyne/__init__.py
+++ pyne/__init__.py
@@ -11,6 +11,11 @@ from . import sound
 
 __version__ = '0.1.0 alpha'
 
+print("\n  // \\")
+print(" //   \\")
+print(" //   \\")
+print("//_____\\")
+print("  ||_|")
 print("\nWelcome to the Pyne")
 print(f"Pyne {__version__} (pygame {pg.__version__}, "
-      f"Python {sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro})")
+      f"Python {sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro})\n")

c5a918fdb7d694a1960c3f550b76ca3d3be58bdf
nchistov <nikola-chistov@mail.ru>
2022-04-04 14:29:34 +0300

    add GameGUIController

---
+++ pyne/game_gui_controller.py
@@ -0,0 +1,33 @@
+import pygame as pg
+
+from pyne.app import NoSouchItemError
+
+
+class GameGUIController:
+    def __init__(self, screen: pg.Surface):
+        self.screen = screen
+
+        self.widgets = []
+
+    def add_widget(self, widget, x, y, width, height, priority=None):
+        widget.set_rect(x, y, width, height)
+
+        if priority is None:
+            self.widgets.append(widget)
+            return
+
+        self.widgets.insert(priority, widget)
+
+    def remove_widget(self, widget):
+        if widget in self.widgets:
+            self.widgets.remove(widget)
+        else:
+            raise NoSouchItemError(f'can not find widget {widget} in widgets.')
+
+    def update(self, event):
+        for widget in self.widgets:
+            widget.update(event)
+
+    def draw(self):
+        for widget in self.widgets:
+            widget.draw(self.screen)

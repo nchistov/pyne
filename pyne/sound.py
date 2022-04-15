5d21413dca0c1df5fd3197f1e2681553e4702c2c
nchistov <nikola-chistov@mail.ru>
2022-04-15 17:23:08 +0300

    add property and setter to class Sound

--- pyne/sound.py
+++ pyne/sound.py
@@ -5,14 +5,21 @@ class Sound:
     def __init__(self, file_name):
         self.sound = pg.mixer.Sound(file_name)
 
-        self.volume = self.sound.get_volume()
+        self._volume = self.sound.get_volume()
 
         self.length = self.sound.get_length()
+
+    @property
+    def volume(self):
+        return self._volume
+
+    @volume.setter
+    def volume(self, value):
+        self._volume = value
+        self.sound.set_volume(self._volume)
 
     def stop(self):
         self.sound.stop()
 
     def play(self):
-        self.sound.set_volume(self.volume)
-
         self.sound.play()

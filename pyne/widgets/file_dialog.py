ec12aed0b7a4decd715e2f77a658bbb98b3558d0
nchistov <nikola-chistov@mail.ru>
2022-04-18 11:56:00 +0300

    add filedialog scrolling and view files

--- pyne/widgets/file_dialog.py
+++ pyne/widgets/file_dialog.py
@@ -1,3 +1,5 @@
+import os
+
 import pygame as pg
 
 from . import Button, Entry, Canvas
@@ -8,12 +10,35 @@ class FileDialog(Widget):
     def __init__(self):
         super().__init__()
 
+        self._speed = 10
+        self._min_y = 5
+        self._max_y = 0
+        self._top_y = 5
+        self._bottom_y = 5
+
         self.save_or_open_btn = Button(text='Open', font_size=30)
         self.cancel_btn = Button(text='Cancel', font_size=30)
 
         self.filename_entry = Entry()
 
         self.canvas = Canvas(outline_color=(100, 100, 100))
+
+        self.canvas_objects = []
+
+    def _read_ls(self, path):
+        """Returns sorted lists of folders and files."""
+        ls_out = os.listdir(path)
+
+        directories = []
+        files = []
+
+        for item in ls_out:
+            if os.path.isdir(os.path.join(path, item)):
+                directories.append(item)
+            else:
+                files.append(item)
+
+        return sorted(directories), sorted(files)
 
     def set_rect(self, x, y, width, height):
         super().set_rect(x, y, width, height)
@@ -24,7 +49,38 @@ class FileDialog(Widget):
 
         self.canvas.set_rect(x + 5, y + 5, width - 10, height - 100)
 
+        result = self._read_ls(os.getcwd())
+        y = 5
+
+        for folder in result[0]:
+            self.canvas_objects.append(self.canvas.draw_text(folder, 25, y, 30))
+            self.canvas_objects.append(self.canvas.draw_image(os.path.join(os.path.dirname(__file__), 'folder.gif'),
+                                                              5, y))
+            y += 40
+            self._max_y = y
+            self._bottom_y = y
+
+        for file in result[1]:
+            self.canvas_objects.append(self.canvas.draw_text(file, 25, y, 30))
+            y += 40
+            self._max_y = y
+            self._bottom_y = y
+
     def update(self, event):
+        if event.type == pg.MOUSEBUTTONDOWN:
+            if event.button == 4:  # Up
+                if self._bottom_y > self.canvas.height:
+                    for obj in self.canvas_objects:
+                        self.canvas.move(obj, 0, -self._speed)
+                    self._top_y -= self._speed
+                    self._bottom_y -= self._speed
+            elif event.button == 5:  # Down
+                if self._top_y < 0:
+                    for obj in self.canvas_objects:
+                        self.canvas.move(obj, 0, self._speed)
+                    self._top_y += self._speed
+                    self._bottom_y += self._speed
+
         for obj in self.save_or_open_btn, self.cancel_btn, self.filename_entry, self.canvas:
             obj.update(event)
 

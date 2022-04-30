8132b180ecde1205e54f7b41f992f3c4e6eef420
nchistov <nikola-chistov@mail.ru>
2022-04-30 14:20:00 +0300

    change version

--- pyne/__init__.py
+++ pyne/__init__.py
@@ -6,6 +6,6 @@ from . import beatle
 from . import sound
 from .game_gui_controller import GameGUIController
 
-__version__ = '0.2.0 beta'
+__version__ = '0.2.0'
 
 print(f'\n[Pyne] version -> {__version__}')

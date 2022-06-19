37cf2a87f22e1d255e2f6d9cc630df4b406939f2
nchistov <nikola-chistov@mail.ru>
2022-06-19 11:44:40 +0300

    change imports in __init__.py

--- pyne/__init__.py
+++ pyne/__init__.py
@@ -6,7 +6,7 @@ from . import beatle
 from . import sound
 from .game_gui_controller import GameGUIController
 from . import errors
-from . import constants
+from .constants import *
 
 __version__ = '0.2.1'
 

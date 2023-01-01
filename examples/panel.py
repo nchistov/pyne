1e0f29299329b90941523f949061f0277ec22e25
nchistov <nikola-chistov@mail.ru>
2023-01-01 12:35:15 +0300

    add uses of some color constants

--- examples/panel.py
+++ examples/panel.py
@@ -4,7 +4,7 @@ app = pyne.App()
 grid = pyne.widgets.Grid(3, 3)
 app.add_widget(grid)
 
-panel = pyne.widgets.Panel((255, 0, 0), (0, 0, 0))
+panel = pyne.widgets.Panel(pyne.RED, pyne.BLACK)
 grid.add_widget(panel, 1, 1)
 
 app.run()

f9abde55c38236adceb3b4fba2d5e61719446fc5
nchistov <nikola-chistov@mail.ru>
2023-03-07 11:31:03 +0300

    add some type annotations

--- pyne/game_gui_controller.py
+++ pyne/game_gui_controller.py
@@ -10,7 +10,7 @@ class GameGUIController:
 
         self.widgets: list[Widget] = []
 
-    def add_widget(self, widget, x, y, width, height, priority=None):
+    def add_widget(self, widget: Widget, x: int, y: int, width: int, height: int, priority: int | None = None):
         widget.set_rect(x, y, width, height)
 
         if priority is None:
@@ -19,13 +19,13 @@ class GameGUIController:
 
         self.widgets.insert(priority, widget)
 
-    def remove_widget(self, widget):
+    def remove_widget(self, widget: Widget):
         if widget in self.widgets:
             self.widgets.remove(widget)
         else:
             raise NoSouchItemError(f'can not find widget {widget} in widgets.')
 
-    def update(self, event):
+    def update(self, event: pg.event.Event):
         for widget in self.widgets:
             widget.update(event)
 

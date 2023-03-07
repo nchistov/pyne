057290d6647e3d8726384233cfdedae69081d957
nchistov <nikola-chistov@mail.ru>
2023-03-07 11:32:12 +0300

    add type annotations to sound.py

--- pyne/sound.py
+++ pyne/sound.py
@@ -2,19 +2,19 @@ import pygame as pg
 
 
 class Sound:
-    def __init__(self, file_name):
+    def __init__(self, file_name: str):
         self.sound = pg.mixer.Sound(file_name)
 
-        self._volume = self.sound.get_volume()
+        self._volume: float = self.sound.get_volume()
 
-        self.length = self.sound.get_length()
+        self.length: float = self.sound.get_length()
 
     @property
     def volume(self):
         return self._volume
 
     @volume.setter
-    def volume(self, value):
+    def volume(self, value: float):
         self._volume = value
         self.sound.set_volume(self._volume)
 
